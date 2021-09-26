447e720757d3cd77d83d476b54dcdf5d16674989
mayssajawadi <jawadimayssa@gmail.com>
2021-09-26 04:23:21 +0100

    Update codes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,6 +15,12 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
+
+
+            <div>
+            <img class="rounded-md mx-auto object-center  md:object-scale-center " src="{{ asset('images/logo.png') }}">
+</div><br>
+
             <div>
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
